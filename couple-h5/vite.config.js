0f3156203444666d26e341c67c25fbf873128019
xinjingzhang <82929568@qq.com>
2026-05-14 10:46:32 +0800

    Update config for deployment

--- couple-h5/vite.config.js
+++ couple-h5/vite.config.js
@@ -4,13 +4,13 @@ import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
 
 export default defineConfig({
-  root: '.',
   plugins: [
     vue(),
     Components({
       resolvers: [VantResolver()],
     }),
   ],
-  // 强制加载 .env 文件
-  envDir: './',
+  build: {
+    outDir: 'dist',
+  },
 })
